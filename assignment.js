b766cb7b0c543b7dd78d9c9569b498f6237227f0
Abhishek8824 <95845677+Abhishek8824@users.noreply.github.com>
2021-12-09 17:55:46 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,16 @@
+describe('my first test suite',function(){
+    it('test cases',function(){
+
+        cy.visit('https://www.gmail.com/')
+        cy.get(".identifierId").type("8824abhishek@gmail.com")
+        cy.get(".VfPpkd-vQzf8d").click()
+
+        })
+        
+       
+
+      
+        
+
+    })
+
